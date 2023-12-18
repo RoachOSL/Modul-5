1ec1bd74dab31808cdcb8a667068a88829e8246c
RoachOSL <os1996@o2.pl>
2023-12-18 15:10:33 +0100

    Added exercise 6

---
+++ test/InvertFileContentTest.java
@@ -0,0 +1,13 @@
+import dev.Roach.InvertFileContent;
+import org.junit.jupiter.api.Test;
+
+public class InvertFileContentTest {
+
+    @Test
+    public void InvertFileContentTestForEmptyInput() {
+        InvertFileContent testObject = new InvertFileContent();
+//        Assertions.assertTrue(testObject.invertFile("ExampleFiles\\example.txt"));
+    }
+
+
+}
